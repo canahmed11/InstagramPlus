3a7a034436a1f2f7ac39d1635d79eafffef3e13e
Ahmet URUNVEREN <urunverenahmett@gmail.com>
2023-06-28 05:47:24 +0300

    done

---
+++ Kotlin Project/3-ArtBookNavigation-master/app/src/main/java/com/atilsamancioglu/artbooknavigation/adapter/ArtListAdapter.kt
@@ -0,0 +1,37 @@
+package com.atilsamancioglu.artbooknavigation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.atilsamancioglu.artbooknavigation.model.Art
+import com.atilsamancioglu.artbooknavigation.databinding.ItemRowBinding
+import com.atilsamancioglu.artbooknavigation.view.ArtListDirections
+
+class ArtListAdapter(val artList : List<Art>) : RecyclerView.Adapter<ArtListAdapter.ArtHolder>() {
+
+    class ArtHolder(val binding : ItemRowBinding): RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtHolder {
+        val binding = ItemRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ArtHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return artList.size
+    }
+
+    override fun onBindViewHolder(holder: ArtHolder, position: Int) {
+
+        holder.binding.artNameText.text = artList[position].artName
+        holder.itemView.setOnClickListener {
+            val action = ArtListDirections.actionArtListToDetailFragment("old",id=artList[position].id)
+            Navigation.findNavController(it).navigate(action)
+
+        }
+    }
+}
+
+
